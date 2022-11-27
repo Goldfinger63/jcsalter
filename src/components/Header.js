b34ec42328a32870f9e2d4798b0fb97872ea377a
jcsalter <jcsalter@sc.edu>
2022-11-27 18:30:29 -0500

    components added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
-import * as React from 'react'
+import React from 'react'
 
+import * as heroStyles from "./hero.module.css"
+import { navigate } from "gatsby"
 
 const Header = () => {
   return (
-    <header>
-      <h1>This is the Header</h1>
+    <header className={heroStyles.hero} >
+       <div>
+       
+       </div>
+       <div className={heroStyles.heroContainer}>
+        <div className={heroStyles.heroInfo}>
+            <h1>Jeff C. Salter</h1>
+            <p>Cybersecurity and Information Design</p>
+            <button className='heroButton' onClick={()=>{navigate("/about")}}>How We Can Help</button>
+        </div>
+       </div>
+     
     </header>
   )
 }
+
 export default Header
